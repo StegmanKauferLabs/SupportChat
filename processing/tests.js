9f48cf2b7b356eba2f81036648c0fca5f4b74ed2
Pierce Stegman <pwstegman@gmail.com>
2016-06-29 16:33:14 -0700

    Added tests

--- processing/tests.js
+++ processing/tests.js
@@ -1,11 +1,35 @@
-var analyse = require("./analyse.js");
-console.log(analyse.chat("Hello"));
+//var natural = require('natural'),
+//  classifier = new natural.BayesClassifier();
 
 var natural = require('natural'),
-  classifier = new natural.BayesClassifier();
+    TfIdf = natural.TfIdf,
+    tfidf = new TfIdf();
+natural.PorterStemmer.attach();
 
-classifier.addDocument('My computer will not turn on', 'plug it in');
+var problems = [
+	"My computer won't turn on",
+	"My screen is shattered",
+	"The screen on my computer is blank"
+];
 
-classifier.train();
+for(var i=0; i<problems.length; i++){
+	var keywords = problems[i].tokenizeAndStem();
+	console.log("Added keywords", keywords);
+	tfidf.addDocument(keywords);
+}
 
+var queries = [
+	"My computer screen is blank",
+	"My harddrive is making a loud noise",
+	"I have a broken screen",
+	"My charger won't work"
+];
 
+for(var i=0; i<queries.length; i++){
+	console.log("-----");	
+	var stems = queries[i].tokenizeAndStem();
+	console.log("Query keywords:", stems);
+	tfidf.tfidfs(stems, function(i, measure) {
+    		console.log('\tproblem #' + i + ' is ' + measure);
+	});
+}
