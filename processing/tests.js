6e13946a154a5d62686cdf9f11d61dafcc75f3f3
Pierce Stegman <pwstegman@gmail.com>
2016-06-29 14:11:19 -0700

    Better slack support

--- processing/tests.js
+++ processing/tests.js
@@ -1,2 +1,11 @@
 var analyse = require("./analyse.js");
-console.log(chat("Hello"));
+console.log(analyse.chat("Hello"));
+
+var natural = require('natural'),
+  classifier = new natural.BayesClassifier();
+
+classifier.addDocument('My computer will not turn on', 'plug it in');
+
+classifier.train();
+
+
