c63bc8d365745844f76754ce394d6e4e0dd98f4d
Matthew Kaufer <mjkaufer@gmail.com>
2016-06-29 19:11:18 -0700

    Test cases

--- processing/chats.js
+++ processing/chats.js
@@ -2,7 +2,17 @@ var chats = [
 	["Hello, my computer will not turn on", "Is it plugged in?", "no", "Okay, plug it in"],
 	["My computer is not turning on", "Is it plugged in?", "yes", "Do you see any lights on your computer which are turned on?", "Yes, there is a green dot near the power button", "Try turning the brightness of the display up"],
 	["Hello, I dropped my phone, and the screen broke", "What kind of a phone is it?", "It is a Google Nexus 5", "Okay, we can repair the screen for $50"],
-	["Hello, I shattered my phone screen", "What kind of a phone is it?", "It is an iPhone 4", "Okay, we can repair the screen for $80"]
+	["Hello, I shattered my phone screen", "What kind of a phone is it?", "It is an iPhone 4", "Okay, we can repair the screen for $80"],
+	["My printer isn't working","Is it plugged into the computer","Yes, I think so","Are the drivers installed","Is this NASCAR","No", "Did you install the printer software","No","Insert the installation disk & install the software"],
+	["My printer isn't working","Is it plugged into the computer","No","Plug it in"],
+	["My house is on fire","Call 911, get the fuck off Slack you dumb shit"],
+	["I don't have internet access","Um bitch guess again you're using Slack aren't you"],
+	["My Mac is stuck in a boot loop","Did you try turning it on and off again?","Yes","Sucks to suck, wipe your computer"],
+	["My Mac is stuck in a boot loop","Did you try turning it on and off again?","No","Do that, it fixes everything"],
+	["I've fallen and I can't get up","Do you have LifeAlert?","no","You're fucked"],
+	["I've fallen and I can't get up","Do you have LifeAlert?","yes","Get off Slack and press the button"],
+	["I tried to make a POST request with JSON data and it didn't work","Is your name Yush?","yes","Get your shit together"],
+	["I tried to make a POST request with JSON data and it didn't work","Is your name Yush?","no","Read the stackoverflow"],
 ];
 
 module.exports = {
